5a668961dc3ae48ae36c103de6c669b8bc353a55
Ahmed El-Sayed Rabie <125227494+AhmedElsayedRabe33@users.noreply.github.com>
2026-07-13 06:18:56 +0300

    Time: 1 ms (7.62%), Space: 8.4 MB (18.24%) - LeetHub

---
+++ 1212-sequential-digits/1212-sequential-digits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> me;
+        queue<int> q;
+        map<int, int> vis;
+        for (int i = 1; i <= 8; i++) {
+            me.push_back(i);
+            q.push(i);
+            vis[i] = 1;
+        }
+        while (!q.empty()) {
+            auto f = q.front();
+            q.pop();
+            int x = f % 10;
+            if (f < 1e9 and x!=9) {
+                f = (f * 10) + (x + 1);
+                if (vis.find(f) == vis.end()) {
+                    q.push(f);
+                    me.push_back(f);
+                    vis[f] = 1;
+                }
+            }
+        }
+        sort(me.begin(), me.end());
+        me.erase(unique(me.begin(), me.end()), me.end());
+        auto l = lower_bound(me.begin(), me.end(), low) - me.begin();
+        auto r = upper_bound(me.begin(), me.end(), high) - me.begin();
+        vector<int> ans;
+        for (int i = l; i < r; i++) {
+            ans.push_back(me[i]);
+        }
+        return ans;
+    }
+};
