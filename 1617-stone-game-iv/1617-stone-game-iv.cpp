bb6b6519de51c89655486db2f1ce694248a7194f
Ahmed El-Sayed Rabie <125227494+AhmedElsayedRabe33@users.noreply.github.com>
2026-08-10 05:48:16 +0300

    Time: 131 ms (22.24%), Space: 9 MB (73.64%) - LeetHub

--- 1617-stone-game-iv/1617-stone-game-iv.cpp
+++ 1617-stone-game-iv/1617-stone-game-iv.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-        vector<int>dp(n+2,-1);
-       auto calc=[&](auto &&calc, int me)->int{
-            if(me==0){
-                return false;
-            }
-            int &ret = dp[me];
-            if(~ret)
-            return ret;
-            ret = 0;
-            for(int i =1 ; i*i<=me;i++){
-                if(calc(calc,me - i*i)==false){
-                    ret = 1;
+        vector<bool>dp(n+2,0);
+        for(int i =1; i<=n ;i++){
+            for(int k = 1 ; k*k <=i ;k++){
+                if(dp[i-k*k]==false){
+                    dp[i]=true;
                 }
             }
-            return ret;
-       };
-       bool ans = calc(calc,n);
-       return ans;
+        }
+        return dp[n];
     }
 };
